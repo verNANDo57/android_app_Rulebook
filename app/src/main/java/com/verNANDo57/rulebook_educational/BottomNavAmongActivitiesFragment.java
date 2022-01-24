c89ccd09be95526b7ae56a5dbd724b6b7c9ce672
verNANDo57 <silvenation@gmail.com>
2022-01-24 19:54:48 +0700

    app: use same fragment layout for every BottomSheetDialogFragment

--- app/src/main/java/com/verNANDo57/rulebook_educational/BottomNavAmongActivitiesFragment.java
+++ app/src/main/java/com/verNANDo57/rulebook_educational/BottomNavAmongActivitiesFragment.java
@@ -1,7 +1,6 @@
 package com.verNANDo57.rulebook_educational;
 
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +13,6 @@ import com.verNANDo57.rulebook_educational.extradata.R;
 import com.verNANDo57.rulebook_educational.preferences.AppSettingsActivity;
 import com.verNANDo57.rulebook_educational.rules.AppDictionaries;
 import com.verNANDo57.rulebook_educational.search.AppSearchActivity;
-import com.verNANDo57.rulebook_educational.utils.ColorUtils;
 
 public class BottomNavAmongActivitiesFragment extends BottomSheetDialogFragment {
 
@@ -23,13 +21,16 @@ public class BottomNavAmongActivitiesFragment extends BottomSheetDialogFragment 
     }
 
     public View onCreateView(LayoutInflater inflater , ViewGroup container, Bundle savedInstanceState) {
-        View viewFragment = inflater.inflate(R.layout.fragment_bottomsheet_among_activities, container, false);
+        View viewFragment = inflater.inflate(R.layout.fragment_bottomsheet_navigationview_content, container, false);
 
-        NavigationView navigationbetweenlessons = viewFragment.findViewById(R.id.navigation_view_among_activities);
-        navigationbetweenlessons.setBackgroundColor(getResources().getColor(R.color.app_rulebook_background));
-        navigationbetweenlessons.setItemTextColor(ColorStateList.valueOf(ColorUtils.lighter(getResources().getColor(R.color.bottomsheetdialog_primary), 0.01f)));
-        navigationbetweenlessons.setItemIconTintList(ColorStateList.valueOf(ColorUtils.lighter(getResources().getColor(R.color.bottomsheetdialog_accent), 0.01f)));
-        navigationbetweenlessons.setNavigationItemSelectedListener(menuItem -> {
+        NavigationView navigation_between_lessons = viewFragment.findViewById(R.id.navigation_view);
+
+        // Clear and inflate menu from resource
+        navigation_between_lessons.getMenu().clear();
+        navigation_between_lessons.inflateMenu(R.menu.bottom_nav_drawer_menu_among_activities);
+
+        // Set onClickListener
+        navigation_between_lessons.setNavigationItemSelectedListener(menuItem -> {
             int id = menuItem.getItemId();
             String className = requireActivity().getClass().getName();
 
--- app/src/main/java/com/verNANDo57/rulebook_educational/BottomNavAmongLessonsFragment.java
+++ app/src/main/java/com/verNANDo57/rulebook_educational/BottomNavAmongLessonsFragment.java
@@ -1,7 +1,6 @@
 package com.verNANDo57.rulebook_educational;
 
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +16,6 @@ import com.verNANDo57.rulebook_educational.rules.AppLexiconActivity;
 import com.verNANDo57.rulebook_educational.rules.AppRulesInSchemesAndTablesActivity;
 import com.verNANDo57.rulebook_educational.rules.mainrules.AppMainRulesActivity;
 import com.verNANDo57.rulebook_educational.search.AppSearchActivity;
-import com.verNANDo57.rulebook_educational.utils.ColorUtils;
 
 public class BottomNavAmongLessonsFragment extends BottomSheetDialogFragment {
 
@@ -26,12 +24,15 @@ public class BottomNavAmongLessonsFragment extends BottomSheetDialogFragment {
     }
 
     public View onCreateView(LayoutInflater inflater , ViewGroup container, Bundle savedInstanceState) {
-        View viewFragment = inflater.inflate(R.layout.fragment_bottomsheet_among_lessons, container, false);
+        View viewFragment = inflater.inflate(R.layout.fragment_bottomsheet_navigationview_content, container, false);
 
-        NavigationView navigation_among_activities = viewFragment.findViewById(R.id.navigation_view_among_lessons);
-        navigation_among_activities.setBackgroundColor(getResources().getColor(R.color.app_rulebook_background));
-        navigation_among_activities.setItemTextColor(ColorStateList.valueOf(ColorUtils.lighter(getResources().getColor(R.color.bottomsheetdialog_primary), 0.01f)));
-        navigation_among_activities.setItemIconTintList(ColorStateList.valueOf(ColorUtils.lighter(getResources().getColor(R.color.bottomsheetdialog_accent), 0.01f)));
+        NavigationView navigation_among_activities = viewFragment.findViewById(R.id.navigation_view);
+
+        // Clear and inflate menu from resource
+        navigation_among_activities.getMenu().clear();
+        navigation_among_activities.inflateMenu(R.menu.bottom_nav_drawer_menu_among_fragments);
+
+        // Set onClickListener
         navigation_among_activities.setNavigationItemSelectedListener(menuItem -> {
             int id = menuItem.getItemId();
             String className = requireActivity().getClass().getName();
